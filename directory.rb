fae34998f9d9369cc81ec6d1ad6437cdeb5b90b8
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-05-10 17:10:37 +0100

    extract process switch statement to method

--- directory.rb
+++ directory.rb
@@ -64,19 +64,20 @@ end
 def interactive_menu
   loop do
     print_menu
-    selection = gets.chomp
+    process(gets.chomp)
+  end
+end
 
-    case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit
-    else
-      puts_centered("I don't know what you meant, try again")
-    end
-
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts_centered("I don't know what you meant, try again")
   end
 end
 
