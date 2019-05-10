504e04326f65efd707a2963d0f13e4f5e4494a1e
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-05-10 10:54:33 +0100

    Receive user input for list of students

--- directory.rb
+++ directory.rb
@@ -13,6 +13,24 @@ students = [
    {name: "Norman Bates", cohort: :november}
 ]
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+
+  name = gets.chomp
+
+  until name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+
+  students
+end
+  
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -26,6 +44,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} excellent students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
